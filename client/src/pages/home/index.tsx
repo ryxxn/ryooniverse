35977a5b16c191cb45ea89505811ca57dc9581f1
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-07 02:32:59 +0900

    [client] feat: restructure HomePage layout for improved responsiveness

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -10,14 +10,16 @@ export default function HomePage() {
   if (isError) return <div>Error occurred</div>;
 
   return (
-    <div className="p-12 flex justify-center items-center gap-4 bg-black h-screen">
-      <CanvasStage />
+    <div className="p-12 flex justify-center items-center bg-black h-screen">
+      <main className="max-w-[1280px] flex gap-4 h-full items-center">
+        <CanvasStage />
 
-      <div className="flex flex-col flex-1 h-full max-h-[600px]">
-        {/* 캐릭터 수정 섹션 */}
-        <UserInfo />
-        <Guestbooks />
-      </div>
+        <div className="flex flex-col flex-1 h-full max-h-[600px]">
+          {/* 캐릭터 수정 섹션 */}
+          <UserInfo />
+          <Guestbooks />
+        </div>
+      </main>
     </div>
   );
 }
