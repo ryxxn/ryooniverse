8af3ef3f3d53a5d019fefbc511522ef1e633108e
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-07 01:47:39 +0900

    [client] feat: temporal add HomePage

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,9 +1,23 @@
-import CanvasStage from "./components/canvas";
+import { useEnterUser } from '../../shared/apis/users/enter';
+import CanvasStage from './components/canvas';
+import Guestbooks from './components/guestbook';
+import UserInfo from './components/user-info';
 
 export default function HomePage() {
+  const { isLoading, isError } = useEnterUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error occurred</div>;
+
   return (
-    <div className='flex justify-center items-center bg-black h-screen'>
-        <CanvasStage />
+    <div className="p-12 flex justify-center items-center gap-4 bg-black h-screen">
+      <CanvasStage />
+
+      <div className="flex flex-col flex-1 h-full max-h-[600px]">
+        {/* 캐릭터 수정 섹션 */}
+        <UserInfo />
+        <Guestbooks />
+      </div>
     </div>
   );
 }
