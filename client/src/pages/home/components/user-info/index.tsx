917c1a0c86e258b937247d48afae13a7a82e0f35
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-07 02:26:54 +0900

    [client] feat: add character selection functionality in UserInfo component

--- client/src/pages/home/components/user-info/index.tsx
+++ client/src/pages/home/components/user-info/index.tsx
@@ -1,13 +1,38 @@
+import { useQueryClient } from '@tanstack/react-query';
+import { queryKeyFactory } from '../../../../shared/apis/query-key-factory';
+
 const CHARACTERS = ['1', '2', '3', '4', '5'];
 
 const UserInfo = () => {
+  /**
+   * ! 임시로 로컬에서만처리
+   * 추후 배치 처리 예정
+   */
+  const queryClient = useQueryClient();
+
+  const onChangeCharacter = (character: string) => {
+    queryClient.setQueryData(queryKeyFactory.users.me, (oldData: any) => {
+      if (!oldData) return oldData;
+
+      return {
+        ...oldData,
+        character,
+      };
+    });
+  };
+
   return (
     <section className="bg-white p-4 rounded-lg shadow-lg">
       <h2 className="text-lg font-bold mb-2">캐릭터 변경</h2>
 
       <div className="flex gap-2">
         {CHARACTERS.map((character) => (
-          <div key={character} className="flex items-center mb-2">
+          <div
+            key={character}
+            role="button"
+            className="flex items-center mb-2"
+            onClick={() => onChangeCharacter(character)}
+          >
             <img
               src={`/assets/characters/${character}.png`}
               alt={character}
