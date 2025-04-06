0c7e58adbe2ef0e2788a9fccb47e049dea689c81
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-07 01:45:09 +0900

    [client] feat: add user entry API query

---
+++ client/src/shared/apis/users/enter.ts
@@ -0,0 +1,33 @@
+import { PATH_API } from '../../constants/api-path';
+import { IUser } from '../../types';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { queryKeyFactory } from '../query-key-factory';
+
+export const enterUser = async () => {
+  const response = await fetch(PATH_API.users.enter, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to enter user');
+  }
+
+  return response.json();
+};
+
+// query
+export const useEnterUser = (
+  other?: Omit<UseQueryOptions<Partial<IUser>, unknown, unknown>, 'queryKey'>
+) => {
+  return useQuery({
+    queryKey: queryKeyFactory.users.me,
+    queryFn: enterUser,
+    gcTime: Infinity,
+    staleTime: Infinity,
+    ...other,
+  });
+};
