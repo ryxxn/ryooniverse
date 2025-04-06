c3b7bd17b837467a5a9a276b901a16771f15b505
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-06 22:16:51 +0900

    [client] feat: add HomePage component

---
+++ client/src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import CanvasStage from "./components/canvas";
+
+export default function HomePage() {
+  return (
+    <div className='flex justify-center items-center bg-black h-screen'>
+        <CanvasStage />
+    </div>
+  );
+}
