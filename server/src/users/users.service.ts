5d6ac7fd55d4d80f49f4011353cd3171654f7b9f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-06 18:27:27 +0900

    feat(server/users): add user enter service

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { AccessLogService } from 'src/access-log/access-log.service';
+import { generateCharacter } from 'src/lib/constants/utils';
+import { generateRandomNickname } from 'src/lib/constants/utils/generate-random-nickname';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    private prisma: PrismaService,
+    private accessLogService: AccessLogService,
+  ) {}
+
+  async enter(ip: string, userId?: string) {
+    let user;
+
+    if (userId) {
+      user = await this.prisma.user.findUnique({ where: { id: userId } });
+    }
+
+    if (!user) {
+      user = await this.prisma.user.create({
+        data: {
+          ipAddress: ip,
+          username: generateRandomNickname(),
+          character: generateCharacter(),
+        },
+      });
+    }
+
+    await this.accessLogService.logAccess(user.id, ip);
+    return user;
+  }
+}
