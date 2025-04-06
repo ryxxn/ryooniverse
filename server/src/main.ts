f7fd88bd5c0e453c078815fddbc7acaeb4789f89
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-06 19:32:38 +0900

    feat(server): set global API prefix and enable CORS in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { PrismaService } from 'prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set global prefix
+  app.setGlobalPrefix('api/v1');
+
+  // Enable CORS
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // prisma
+  const prismaService = app.get(PrismaService);
+  prismaService.enableShutdownHook(app);
+
   app.use(cookieParser());
   await app.listen(8777);
 }
