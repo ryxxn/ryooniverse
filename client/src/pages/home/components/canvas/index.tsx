fb3d0eb60102a62c3d940baf7fa405d355fc1915
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-07 02:26:43 +0900

    [client] feat: integrate user data into canvas component and update character loading

--- client/src/pages/home/components/canvas/index.tsx
+++ client/src/pages/home/components/canvas/index.tsx
@@ -1,27 +1,54 @@
-import { useEffect, useRef } from "react";
-import { CANVAS_CONSTANT } from "./canvas.constants";
-import { setupInput, loadCharacterImage, loadMap, startGameLoop } from "./canvas.utils";
+import { useEffect, useRef } from 'react';
+import { CANVAS_CONSTANT } from './canvas.constants';
+import {
+  setupInput,
+  loadCharacterImage,
+  loadMap,
+  startGameLoop,
+} from './canvas.utils';
+import { useEnterUser } from '../../../../shared/apis/users/enter';
+import { useCanvasStore } from './canvas.store';
 
 export default function CanvasStage() {
+  const { data } = useEnterUser({ enabled: false });
+  const { x, y } = useCanvasStore((state) => state.myCharacter);
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const ctx = canvasRef.current?.getContext("2d");
+    const ctx = canvasRef.current?.getContext('2d');
     if (!ctx) return;
 
     const cleanup = setupInput();
 
-    Promise.all([loadCharacterImage(), loadMap()]).then(() => {
-      startGameLoop(ctx);
-    });
+    Promise.all([loadCharacterImage(data?.character ?? '1'), loadMap()]).then(
+      () => {
+        startGameLoop(ctx);
+      }
+    );
 
     return cleanup;
-  }, []);
+  }, [data]);
 
   return (
-    <canvas
-      ref={canvasRef}
-      width={CANVAS_CONSTANT.MAP_WIDTH}
-      height={CANVAS_CONSTANT.MAP_HEIGHT}
-    />);
+    <div className="relative">
+      <canvas
+        ref={canvasRef}
+        width={CANVAS_CONSTANT.MAP_WIDTH}
+        height={CANVAS_CONSTANT.MAP_HEIGHT}
+      />
+      <div
+        className="absolute text-white text-sm font-bold"
+        style={{
+          top: y + 100,
+          left: x + 50,
+          transform: 'translateX(-50%) translateY(-50%)',
+        }}
+      >
+        <div className="bg-black bg-opacity-60 px-2 py-1 rounded-lg">
+          {data?.username}
+        </div>
+      </div>
+    </div>
+  );
 }
