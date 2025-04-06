2dc15284dd77897533a71bdb6606e47fe129321d
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-06 22:45:56 +0900

    [server] feat: add update method to UsersService for user data modification

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from 'prisma/prisma.service';
 import { AccessLogService } from 'src/access-log/access-log.service';
 import { generateCharacter } from 'src/lib/constants/utils';
 import { generateRandomNickname } from 'src/lib/constants/utils/generate-random-nickname';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
@@ -29,6 +30,18 @@ export class UsersService {
     }
 
     await this.accessLogService.logAccess(user.id, ip);
+    return user;
+  }
+
+  async update(userId: string, body: Partial<UserDto>) {
+    const { username, character } = body;
+    const user = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        username,
+        character,
+      },
+    });
     return user;
   }
 }
