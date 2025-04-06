a13501a60d3232924ef3e9033ee427f2c2fd7c78
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-07 01:44:36 +0900

    [client] feat: wrap application in ReactQueryProvider and ToastProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
+import './App.css';
 
 import HomePage from './pages/home';
 import WelcomePage from './pages/wellcome';
+import { ReactQueryProvider, ToastProvider } from './shared/providers';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <ToastProvider>
+      <ReactQueryProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/home" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
+      </ReactQueryProvider>
+    </ToastProvider>
   );
 }
-
