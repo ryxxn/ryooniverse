0f0b5341f1501aeea5c6a8ae5d91816fb894148b
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-06 22:10:47 +0900

    [client] chore: add Tailwind CSS plugin and configure server settings

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0', // 모든 네트워크 인터페이스에서 요청 수신
+    // proxy: {
+    //   '/api': {
+    //     target: '',
+    //     changeOrigin: true,
+    //   },
+    // },
+  },
 })
