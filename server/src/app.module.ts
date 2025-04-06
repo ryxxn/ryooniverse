4b05a7a58f8ea4f3610f560e3bdc6885cdf4ee02
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-06 18:24:50 +0900

    feat(server): remove AppController tests and update app module with new modules and global config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WsModule } from './gateway/ws.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AccessLogModule } from './access-log/access-log.module';
 
 @Module({
-  imports: [],
+  imports: [
+    WsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    AccessLogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
