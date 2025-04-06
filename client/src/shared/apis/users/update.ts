06e06ef1e2a76884f51b27b3541bb2eb59534260
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-07 01:44:59 +0900

    [client] feat: add update user API query

---
+++ client/src/shared/apis/users/update.ts
@@ -0,0 +1,33 @@
+import { PATH_API } from '../../constants/api-path';
+import { IUser } from '../../types';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+
+export const updateUser = async (data: Partial<IUser>) => {
+  const response = await fetch(PATH_API.users.update, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to update user');
+  }
+
+  return response.json();
+};
+
+// query
+export const useUpdateUser = (
+  options?: Omit<
+    UseMutationOptions<Partial<IUser>, unknown, unknown>,
+    'mutationKey'
+  >
+) => {
+  return useMutation({
+    mutationFn: updateUser,
+    ...options,
+  });
+};
