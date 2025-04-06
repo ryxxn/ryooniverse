2a8211cedeb564170ce079056041f915622b9826
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-07 01:59:21 +0900

    [client] feat: add redirect from all routes to HomePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/home';
@@ -11,6 +11,7 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/*" element={<Navigate to="/" replace />} />
           </Routes>
         </BrowserRouter>
       </ReactQueryProvider>
