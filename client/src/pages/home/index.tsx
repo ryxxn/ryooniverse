eefeba825441da6db5fe1da21b708c673526768f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-08 15:10:51 +0900

    [client] feat: adjust layout width for HomePage component

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
 
   return (
     <div className="p-12 flex justify-center items-center bg-black h-screen">
-      <main className="max-w-[1280px] flex gap-4 h-full items-center">
+      <main className="min-w-6xl max-w-7xl flex gap-4 h-full items-center">
         <CanvasStage />
 
         <div className="flex flex-col flex-1 h-full max-h-[600px]">
