54440f3a5e755d6afae1da5b672c9f965c904760
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-10 22:27:21 +0900

    [client] fix: configure socket path based on production environment

--- client/src/shared/utils/socket.ts
+++ client/src/shared/utils/socket.ts
@@ -1,5 +1,8 @@
 import { io, Socket } from 'socket.io-client';
 
+const isProduction = import.meta.env.MODE === 'production';
+
 export const socket: Socket = io(import.meta.env.VITE_WS_URL, {
+  path: isProduction ? '/ryooniverse/socket.io' : '/socket.io',
   transports: ['websocket'],
 });
